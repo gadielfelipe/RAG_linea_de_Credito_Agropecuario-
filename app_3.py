5355f1e7474580a730db6b01f1fc04fe6126be53
Gadiel Castillo <72670480+gadielfelipe@users.noreply.github.com>
2025-04-25 14:56:09 -0500

    app_3.py

--- app_3.py
+++ app_3.py
@@ -10,7 +10,6 @@ API_KEY = "AIzaSyAD7fHhvjm4VmGZI1AjXsmNlkXeluit5a4"
 GEMINI_URL = f"https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash:generateContent?key={API_KEY}"
 HEADERS = {"Content-Type": "application/json"}
 
-# --- Function to Extract Text from PDF ---
 def extract_text_from_pdf(pdf_path):
     text = ""
     with fitz.open(pdf_path) as doc:
@@ -23,7 +22,6 @@ pdf_text = extract_text_from_pdf("Resolucion2025.pdf")
 st.image("logo_finagro.png", width=200)
 
 st.title("¿Necesitas información sobre la Línea ANT de Finagro? Pregunta aquí sobre requisitos, condiciones y beneficiarios.")
-
 
 question = st.text_input("Ingresa tu pregunta sobre la Línea ANT de Finagro:", placeholder="Ejemplo: ¿Cuáles son los requisitos para acceder?")
 
@@ -58,7 +56,6 @@ if st.button("Obtener respuesta") and question:
                 st.error("Error en la respuesta. Intente nuevamente más tarde.")
         else:
             st.error("Error: No se pudo obtener una respuesta. Intente nuevamente más tarde.")
-
 
 if 'first_visit' not in st.session_state:
     st.session_state['first_visit'] = True
